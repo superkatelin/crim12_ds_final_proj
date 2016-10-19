a2facbbf999bde5e9796ff02657b36247ae6e10b
superkatelin <katelin.corbin@hotmail.com>
2016-10-19 10:54:14 -0400

    Update sensor.php

--- app/models/sensor.php
+++ app/models/sensor.php
@@ -1,15 +1,17 @@
 <?php
 
-public $sensorId;
-public $sensorName;
-public $sensorDescription;
-public $manufacturer;
-public $totalLifeExpectancyHours;
+class sensor{
+    public $sensorId;
+    public $sensorName;
+    public $sensorDescription;
+    public $manufacturer;
+    public $totalLifeExpectancyHours;
 
-public function __construct($sensorId,$sensorName,$sensorDescription,$manufacturer,$totalLifeExpectancyHours) {
-    $this->sensorId = $sensorId;
-    $this->sensorName = $sensorName;
-    $this->sensorDescription = $sensorDescription;
-    $this->manufacturer = $manufacturer;
-    $this->totalLifeExpectancyHours = $totalLifeExpectancyHours;
+    public function __construct($sensorId,$sensorName,$sensorDescription,$manufacturer,$totalLifeExpectancyHours) {
+        $this->sensorId = $sensorId;
+        $this->sensorName = $sensorName;
+        $this->sensorDescription = $sensorDescription;
+        $this->manufacturer = $manufacturer;
+        $this->totalLifeExpectancyHours = $totalLifeExpectancyHours;
+    }
 }
