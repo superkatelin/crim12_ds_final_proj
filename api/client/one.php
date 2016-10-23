503093195a845be90b20d3fc41472c101bd24b2b
superkatelin <katelin.corbin@hotmail.com>
2016-10-22 21:01:50 -0400

    Create one.php

---
+++ api/client/one.php
@@ -0,0 +1,37 @@
+<?php
+
+require '../../app/environment.php';
+
+$db = new PDO(DB_CONNECT, DB_USER, DB_PASSWORD);
+
+$sql = 'SELECT * FROM client WHERE clientId = ?';
+
+$statement = $db->prepare($sql);
+
+$success = $statement->execute(
+    array(
+        $_GET['clientId']
+    )
+);
+
+if (! $success) {
+    header('HTTP/1.1 500 Error');
+    var_dump($statement->errorInfo());
+    exit('Bad SQL');
+}
+
+$arr = array();
+
+while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+    $client = new client(
+        $row['clientId'],
+        $row['clientName'],
+        $row['clientDescription'],
+        $row['gicsSector'],
+        $row['gicsSubIndustry'],
+        $row['headquarters']
+    );
+    array_push($arr, $client);
+}
+
+echo json_encode($arr);
